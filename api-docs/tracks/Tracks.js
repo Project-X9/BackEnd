c1320883f069cf652762436660ebaee8d402d0c4
Sondos-elmoataz <sondos.Hassan98@eng-st.cu.edu.eg>
2020-03-13 21:38:18 +0200

    updated by Sondos 13/3

--- api-docs/tracks/Tracks.js
+++ api-docs/tracks/Tracks.js
@@ -1,35 +1,33 @@
 /**
  * @api {get} /tracks/{id}
+ * @apiGroup Tracks
  * @apiName getTrack 	Get a Track
- * @apiHeader {Authorization} Required. A valid access token from the Spotify Accounts service: see the Web API Authorization Guide for details.
- * @apiParam {string} 	The Spotify ID for the track.
- * @apiParam (Query parameters) {market} Optional. An ISO 3166-1 alpha-2 country code or the string from_token. Provide this parameter if you want to apply Track Relinking.
+ * @apiDescription Get Spotify catalog information for a single track identified by its unique Spotify ID.
+ * @apiHeader {String} Authorization (Required.) A valid access token from the Spotify Accounts service: see the Web API Authorization Guide for details.
+ * @apiParam {string} id	The Spotify ID for the track.
+ * @apiParam (Query parameters) {market} (Optional.) An ISO 3166-1 alpha-2 country code or the string from_token. Provide this parameter if you want to apply Track Relinking.
  * @apiSuccess {Object} track
  * @apiSuccessExample Success-Response:
  *      HTTP/1.1 200 OK
  *          {
+ *              "track"
  *          }
  */
-function getTrack()
-{
-    return {code: 200, data: trackObject}
-
-}
 //-----------------------------------------------------------
 /**
  * @api {get} /tracks
+ * @apiGroup Tracks
  * @apiName getTracks Get Several Tracks
+ * @apiDescription Get Spotify catalog information for multiple tracks based on their Spotify IDs.
  * @apiSuccess {Object[]} tracks
- * @apiParam (Query Parameters) {ids} Required. A comma-separated list of the Spotify IDs for the tracks. Maximum: 50 IDs.
- * @apiParam (Quer Parameters) {market} 	Optional. An ISO 3166-1 alpha-2 country code or the string from_token. Provide this parameter if you want to apply Track Relinking.
+ * @apiParam (Query Parameters) {String[]} ids (Required) A comma-separated list of the Spotify IDs for the tracks. Maximum: 50 IDs.
+ * @apiParam (Quer Parameters) {String} market 	(Optional) An ISO 3166-1 alpha-2 country code or the string from_token. Provide this parameter if you want to apply Track Relinking.
+ * @apiHeader {String} Authorization  (required) A valid user access token.
  * @apiSuccessExample Success-Example:
  *       HTTP/1.1 200 OK
  *          {
+ *              "tracks" = {Object[]}
  *          }
  * 
  * 
  */
-function getTracks()
-{
-    return {code: 200, data: trackObject}
-}
