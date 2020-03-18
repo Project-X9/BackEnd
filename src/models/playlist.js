cf7663406a74734de14668948bedd15376b32145
Ruba-Ashraf <61359028+Ruba-Ashraf@users.noreply.github.com>
2020-03-18 20:47:06 +0200

    Update playlist.js

--- src/models/playlist.js
+++ src/models/playlist.js
@@ -1,1 +1,20 @@
 const mongoose = require('mongoose');
+
+const playlistSchema = mongoose.Schema({
+    name: String,
+    author: String,
+    artists: [String],
+    trackIds: [String],
+    discription: String,
+    image: String,
+    dateCreated:{
+        type: Date,
+        default: Date.now()
+    },
+    followers: [String],
+    Is_private: Boolean
+
+});
+
+const Playlist= mongoose.model('Playist', playlistSchema);
+module.exports = Playlist;
