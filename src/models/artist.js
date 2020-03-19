b00515cf14df85ff9ba1a9b1d9376593a4a30b26
Sondos-elmoataz <sondos.Hassan98@eng-st.cu.edu.eg>
2020-03-19 03:51:31 +0200

    updated by Sondos

--- src/models/artist.js
+++ src/models/artist.js
@@ -1,1 +1,18 @@
 const mongoose = require('mongoose');
+
+const artistSchema = new mongoose.Schema({
+
+    external_url : [String],
+    followers : [String],
+    genres :{type:[String],default:[]},
+    href: String,
+    id: String,
+    images: [String],
+    name: String,
+    popularity: Number,
+    uri: String
+
+});
+
+const Artist = mongoose.model('Artist',trackSchema);
+module.exports = Artist;
--- src/models/track.js
+++ src/models/track.js
@@ -1,1 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose= require('mongoose');
+
+
+const trackSchema = new mongoose.Schema({
+    album :{type: mongoose.Schema.Types.ObjectId,ref:'Album'},
+    artists :[{type: mongoose.Schema.Types.ObjectId,ref:'Artist'}],
+    disc_number : Number,
+    duration_ms : Number,
+    explicit: Boolean ,
+    href : string,
+    id : string,
+    is_playable : Boolean,
+    name : string,
+    popularity : Number,
+    preview_url : {type:string, default: null},
+    uri : string
+});
+
+const Track = mongoose.model('Track',trackSchema);
+module.exports = Track;
