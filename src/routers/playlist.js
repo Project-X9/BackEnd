90e657ed9c9601b45b32effc4b995dbec89e7533
Ruba-Ashraf <61359028+Ruba-Ashraf@users.noreply.github.com>
2020-04-01 05:18:41 +0200

    Update playlist routes
    still need to check on some functions with other team members

--- src/routers/playlist.js
+++ src/routers/playlist.js
@@ -1,2 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = new express.Router();
+
+const PController = require(`./../controllers/playlistController`); 
+
+router.route("/:id")
+    .get(PController.getPlaylistById)           
+    .delete(PController.deletePlaylist)         
+    .patch(PController.updatePlaylist);     
+
+
+
+router
+  .route("/")
+  .post(playlistController.createPlaylist);
+
+
+
+
+router
+    .route("/tracks")                          
+    .get(PController.getPlaylistTracks)            
+    .delete(PController.deletePlaylistTarck);      
+
+
+//.patch(PController.addPlaylistTracks)           is it needed or is update enough ?
+//     .get(PController.getPlaylistByName);       // what iF i have many with the same name? returns an arr of playlist
+
+module.exports = router;
+
