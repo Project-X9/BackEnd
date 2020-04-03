863edcfaf0523585a694ddedeef2e2190006ff91
Ruba-Ashraf <61359028+Ruba-Ashraf@users.noreply.github.com>
2020-04-03 04:39:37 +0200

    Updated Album Routes

--- src/routers/album.js
+++ src/routers/album.js
@@ -1,2 +1,18 @@
 const express = require('express');
 const router = new express.Router();
+
+const AController = require(`./../controllers/albumController`); 
+
+
+router
+    .route("/")
+    .get(AController.getAllAlbums());
+
+
+router
+    .route("/:id")
+    .get(AController.getAlbumById());
+
+router
+    .route("/:id/tracks")
+    .get(AController.getAlbumTracks());
