96f5a24b84b12ee671c529be571b75e3ecc8e78f
Abdallah Hossam <abdallahakrab98@gmail.com>
2020-04-04 13:53:56 +0200

    reset to before deploy test

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
 const express = require('express')
-const deployRouter = require('./routers/deploy_test')
-require('./db/mongoose')    
+require('./db/mongoose')
 // const userRouter = require('./routers/user')
 // const taskRouter = require('./routers/task')
 
 const app = express()
 
 app.use(express.json())
-app.use("/api",deployRouter);
+// app.use(userRouter)
 // app.use(taskRouter)
 
 module.exports = app
