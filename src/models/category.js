d255adad42ba216cf7760a19ff007b4813815b36
Abdallah Hossam <abdallahakrab98@gmail.com>
2020-04-05 18:17:38 +0200

    category model minor update ( name unique , href not required)

--- src/models/category.js
+++ src/models/category.js
@@ -3,18 +3,17 @@ const mongoose = require('mongoose');
 categorySchema = mongoose.Schema({
     name: {
         type: String,
+        unique: true,
         required: [true, 'A category must have a name'],
         minlength:1,
         maxlength:50
     },
     icon: {
         type: String,
-        minlength:5,
-        maxlength:255
     },
     href:{
         type: String,
-        required: [true, 'A category must have a href'],
+        required: [false, 'A category must have a href'],
         minlength:5,
         maxlength:255
     },
