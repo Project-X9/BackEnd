47f083e24338ee2e81027f1472dacea319900158
shoroukkhaledfarag <shoroukkhaledfarag@hotmail.com>
2020-04-07 19:44:33 +0200

    updated all delete requests to patch requests in follow router -shorouk

--- src/routers/follow.js
+++ src/routers/follow.js
@@ -9,26 +9,41 @@ router
 router
     .route("/user/:id")
     .patch(followController.followUser)
-    .delete(followController.unfollowUser)
+
+router
+    .route("/user/un/:id")    
+    .patch(followController.unfollowUser)
 
 router
     .route("/playlist/:id")
     .patch(followController.likePlaylist)
-    .delete(followController.dislikePlaylist)
+
+router
+    .route("/playlist/un/:id")
+    .patch(followController.dislikePlaylist)
 
 router
     .route("/album/:id")
     .patch(followController.likeAlbum)
-    .delete(followController.dislikeAlbum);
+
+router
+    .route("/album/un/:id")
+    .patch(followController.dislikeAlbum);
 
 router
      .route("/track/:id")
      .patch(followController.likeTracks)
-     .delete(followController.dislikeTracks)
+
+router
+     .route("/track/un/:id")
+     .patch(followController.dislikeTracks)
 
 router
      .route("/artist/:id")
      .patch(followController.likeArtist)
-     .delete(followController.dislikeArtist)
+
+router
+     .route("/artist/un/:id")
+     .patch(followController.dislikeArtist)
 
 module.exports = router;
