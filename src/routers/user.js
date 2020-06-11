96fcde0bfda08185a76a9f7dfcb1b92d93a54c56
hzmmohamed <hzmohamed@gmail.com>
2020-06-11 21:10:11 +0200

    .

--- src/routers/user.js
+++ src/routers/user.js
@@ -34,7 +34,7 @@ router
 
 router
   .route("/:id")
-  .get(auth,userController.getUser)
+  .get(userController.getUser)
   .patch(userController.updateUser)
   .delete(userController.deleteUser);
 
